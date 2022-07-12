1ef1924a1f312be59211f689118c3f6d2b0228c7
Zack Scholl <zack.scholl@gmail.com>
2022-07-12 12:29:58 -0700

    update all

--- install_vim.sh
+++ install_vim.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-sudo apt install -y zsh vim htop pv git python3-pip cmake lua5.3
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-## vim
+rm -rf ~/.vim/pack/plugins/start/vim-go
 git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
 mkdir -p ~/.vim/colors
 curl https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim > ~/.vim/colors/molokai.vim
@@ -13,16 +10,8 @@ curl https://raw.githubusercontent.com/schollz/dotfiles/main/vimrc > ~/.vimrc
 curl https://raw.githubusercontent.com/schollz/dotfiles/main/dircolors.monokai > /tmp/dircolors.monokai
 dircolors /tmp/dircolors.monokai >> ~/.zshrc
 
-## install go
-./install_go.sh
-mkdir -p ~/go/src/github.com/schollz
-
-## install node
-./install_node.sh
-
-## install croc
-curl https://getcroc.schollz.com | bash
-
-## update python
-sudo -H python3 -m pip install --upgrade pip
-sudo -H python3 -m pip install tqdm numpy
+## lua stuff
+curl https://raw.githubusercontent.com/schollz/LuaFormat/master/lua-format.py >  ~/.config/lua-format.py
+echo "set autoread" >> ~/.vimrc
+echo "autocmd BufWritePost *.lua silent! !python3 ~/.config/lua-format.py <afile>" >> ~/.vimrc
+echo "autocmd BufWritePost *.lua redraw!" >> ~/.vimrc
