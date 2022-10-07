2637b80123e0136f803359313ae563d3a77812c1
Zack <zack.scholl@gmail.com>
2022-10-07 12:30:37 -0700

    fix dotfiles

--- install_all.sh
+++ install_all.sh
@@ -4,13 +4,13 @@ sudo apt install -y zsh vim htop pv git python3-pip cmake lua5.3
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 ## vim
-git clone https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
+git clone --depth 1 https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
 mkdir -p ~/.vim/colors
 curl https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim > ~/.vim/colors/molokai.vim
-curl https://raw.githubusercontent.com/schollz/dotfiles/main/vimrc > ~/.vimrc
+curl https://raw.githubusercontent.com/schollz/dotfiles/master/vimrc > ~/.vimrc
 
 ## better ls colors
-curl https://raw.githubusercontent.com/schollz/dotfiles/main/dircolors.monokai > /tmp/dircolors.monokai
+curl https://raw.githubusercontent.com/schollz/dotfiles/master/dircolors.monokai > /tmp/dircolors.monokai
 dircolors /tmp/dircolors.monokai >> ~/.zshrc
 
 ## install go
