96a5b76f8db5f1392efb680734182ee4e159fecf
Zack <zack.scholl@gmail.com>
2022-10-10 13:36:39 -0700

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -2,14 +2,11 @@
 
 sudo apt install -y zsh vim htop pv git python3-pip cmake lua5.3
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-## vim
+mkdir -p ~/.vim/pack/plugins/start
 git clone --depth 1 https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
 mkdir -p ~/.vim/colors
 curl https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim > ~/.vim/colors/molokai.vim
 curl https://raw.githubusercontent.com/schollz/dotfiles/master/vimrc > ~/.vimrc
-
-## better ls colors
 curl https://raw.githubusercontent.com/schollz/dotfiles/master/dircolors.monokai > /tmp/dircolors.monokai
 dircolors /tmp/dircolors.monokai >> ~/.zshrc
 
