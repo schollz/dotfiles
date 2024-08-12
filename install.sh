8d3c0c2934b3dbd766cc2c22a80778af4ee60242
Zack <zack.scholl@gmail.com>
2024-08-12 09:32:10 -0700

    add zsh and go changes

--- install.sh
+++ install.sh
@@ -8,35 +8,47 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 # add zsrhc
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 curl https://raw.githubusercontent.com/schollz/dotfiles/master/zshrc > ~/.zshrc
+zsh 
 source ~/.zshrc
 
 # add lua support
 mkdir -p ~/.config
-curl https://raw.githubusercontent.com/schollz/LuaFormat/master/lua-format.py >  ~/.config/lua-format.py
+curl -s https://raw.githubusercontent.com/schollz/LuaFormat/master/lua-format.py >  ~/.config/lua-format.py
 
 # improve vim
 mkdir -p ~/.vim/pack/plugins/start
 git clone --depth 1 https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
 mkdir -p ~/.vim/colors
-curl https://raw.githubusercontent.com/schollz/dotfiles/main/dircolors.monokai > ~/.vim/colors/molokai.vim
-curl https://raw.githubusercontent.com/schollz/dotfiles/master/vimrc > ~/.vimrc
+curl -s https://raw.githubusercontent.com/schollz/dotfiles/main/dircolors.monokai > ~/.vim/colors/molokai.vim
+curl -s https://raw.githubusercontent.com/schollz/dotfiles/master/vimrc > ~/.vimrc
 ## markdown stuff
-cd ~/.vim && wget https://github.com/preservim/vim-markdown/archive/master.tar.gz && tar --strip=1 -zxf master.tar.gz && rm -rf master.tar.gz
+cd ~/.vim && wget -q https://github.com/preservim/vim-markdown/archive/master.tar.gz && tar --strip=1 -zxf master.tar.gz && rm -rf master.tar.gz
+cd ~
 
 
 # install croc
-curl https://getcroc.schollz.com | bash
+curl -s https://getcroc.schollz.com | bash
 
 # install torrenter
-curl https://gettorrenter.schollz.com | bash
+curl -s https://gettorrenter.schollz.com | bash
 
 # install node 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
 source ~/.zshrc
 nvm install 22
 node -v
 npm -v
 
 # install go 
-curl -LO https://get.golang.org/$(uname)/go_installer && chmod +x go_installer && ./go_installer && rm go_installer
-
+arch="$(uname -m)"
+if [[ "$arch" == *"aarch"* ]]; then
+    url="$(wget -qO- https://golang.org/dl/ | grep -oP 'go([0-9\.]+)\.linux-arm64\.tar\.gz' | sed 's/^/https:\/\/golang.org\/dl\//g' | head -n 1)"
+else
+    url="$(wget -qO- https://golang.org/dl/ | grep -oP 'go([0-9\.]+)\.linux-amd64\.tar\.gz' | sed 's/^/https:\/\/golang.org\/dl\//g' | head -n 1)"
+fi
+echo "downloading: ${url}"
+wget --progress=bar:force --quiet "${url}" -O /tmp/go.tar.gz
+sudo rm -rf /usr/local/go
+sudo tar -C /usr/local -xzf /tmp/go.tar.gz
+rm /tmp/go.tar.gz
+go version
