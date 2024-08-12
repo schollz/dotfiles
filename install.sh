91696913e41f6a7520c95859cc417d1e8a87962b
Zack <zack.scholl@gmail.com>
2024-08-12 09:52:30 -0700

    working

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sudo apt update
-sudo apt install -y zsh vim git clang-format wget curl python3
+sudo apt update -qq
+sudo apt install -y -qq zsh vim git clang-format wget curl python3
 
 # add zsrhc
 # check if .zshrc does not exist
@@ -23,7 +23,7 @@ if [ ! -f ~/.vimrc ]; then
     mkdir -p ~/.vim/pack/plugins/start
     git clone --depth 1 https://github.com/fatih/vim-go.git ~/.vim/pack/plugins/start/vim-go
     mkdir -p ~/.vim/colors
-    curl -s https://dotfiles.schollz.com/dircolors.monokai > ~/.vim/colors/molokai.vim
+    curl https://raw.githubusercontent.com/tomasr/molokai/master/colors/molokai.vim > ~/.vim/colors/molokai.vim
     curl -s https://dotfiles.schollz.com/vimrc > ~/.vimrc
     ## markdown stuff
     cd ~/.vim && wget -q https://github.com/preservim/vim-markdown/archive/master.tar.gz && tar --strip=1 -zxf master.tar.gz && rm -rf master.tar.gz
@@ -44,6 +44,7 @@ zsh
 source ~/.zshrc
 if [ ! -f ~/.nvm/nvm.sh ]; then
     curl -s -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.0/install.sh | bash
+    source ~/.zshrc
     nvm install 22
 fi
 source ~/.zshrc
